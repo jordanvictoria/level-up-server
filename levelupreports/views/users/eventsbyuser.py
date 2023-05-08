a300d2d83750bc217c76ceab3b0c3ea57dc4d77d
Jordan Victoria <jgomeskuehner@gmail.com>
2023-05-08 15:12:02 -0500

    end of book 4 colum

---
+++ levelupreports/views/users/eventsbyuser.py
@@ -0,0 +1,92 @@
+"""Module for generating games by user report"""
+from django.shortcuts import render
+from django.db import connection
+from django.views import View
+
+from levelupreports.views.helpers import dict_fetch_all
+
+
+class UserEventList(View):
+    def get(self, request):
+        with connection.cursor() as db_cursor:
+
+            # TODO: Write a query to get all games along with the gamer first name, last name, and id
+            db_cursor.execute("""
+            SELECT
+                a.id,
+                a.description,
+                a.date,
+                a.time,
+                a.game_id,
+                a.organizer_id,
+                u.id user_id,
+                u.first_name || ' ' || u.last_name AS full_name
+            FROM 
+                levelupapi_event a
+            JOIN
+                levelupapi_gamer g ON a.organizer_id = g.user_id
+            JOIN
+                auth_user u ON g.user_id = u.id
+            """)
+            # Pass the db_cursor to the dict_fetch_all function to turn the fetch_all() response into a dictionary
+            dataset = dict_fetch_all(db_cursor)
+
+            # Take the flat data from the dataset, and build the
+            # following data structure:
+            #
+            # [
+                # {
+                #     "gamer_id": 1,
+                #     "full_name": "Molly Ringwald",
+                #     "events": [
+                #     {
+                #         "id": 5,
+                #         "date": "2020-12-23",
+                #         "time": "19:00",
+                #         "game_name": "Fortress America"
+                #     }
+                #     ]
+                # }
+                # ]
+
+            events_by_user = []
+
+            for row in dataset:
+                # TODO: Create a dictionary called game that includes 
+                # the name, description, game_id, date,
+                # game_type_id, and time from the row dictionary
+                event = {
+                    "description": row['description'],
+                    "date": row['date'],
+                    "time": row['time'],
+                    "game_id": row['game_id'],
+                    "organizer_id": row['organizer_id'],
+                }
+                
+                # See if the gamer has been added to the games_by_user list already
+                user_dict = None
+                for user_event in events_by_user:
+                     if user_event['gamer_id'] == row['organizer_id']:
+                         user_dict = user_event
+                
+                
+                if user_dict:
+                    # If the user_dict is already in the events_by_user list, append the event to the events list
+                    user_dict['events'].append(event)
+                else:
+                    # If the user is not on the events_by_user list, create and add the user to the list
+                    events_by_user.append({
+                        "gamer_id": row['organizer_id'],
+                        "full_name": row['full_name'],
+                        "events": [event]
+                    })
+        
+        # The template string must match the file name of the html template
+        template = 'users/list_with_events.html'
+        
+        # The context will be a dictionary that the template can access to show data
+        context = {
+            "userevent_list": events_by_user
+        }
+
+        return render(request, template, context)
